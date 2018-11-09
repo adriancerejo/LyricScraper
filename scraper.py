dcad1f8c5a1bc849df636f56417a96db81da48db
Adrian <37904908+adriancerejo@users.noreply.github.com>
2018-11-08 19:41:37 -0500

    update 1
    Python searches for the lyrics using azlyrics exclusively and gets the link. It then uses urllib to get the document behind the html to pass into 
    beautifulsoup4's prettify method to make the data readable.

--- scraper.py
+++ scraper.py
@@ -1,13 +1,24 @@
 # google library to search songs
 from googlesearch import search
+# beautiful soup library to make html file readable
+from bs4 import BeautifulSoup
+# urllib lib to request document behind url
+from urllib.request import urlopen
+
 
 # looks up song name on google
 def google_search(look):
     for j in search(look, tld="co.in", num=1, stop=1, pause=2):
+        # stores url into variable
+        url = j
+        # uses bs4 to open the url after urllib gets the doc
+        soup = BeautifulSoup(urlopen(j), 'html.parser')
+        # "prettifies" it (makes it readable) and prints
+        print(soup.prettify())
         print(j)
 
 
-# song name to search
+# song name to search and uses azlyrics.com exclusively
 print("Enter song name: ")
 query = input()
 query += 'azlyrics'
