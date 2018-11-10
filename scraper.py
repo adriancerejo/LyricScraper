fc9b519ef8132d057767e7d451d99ee207cbbcfa
Adrian <37904908+adriancerejo@users.noreply.github.com>
2018-11-10 10:26:35 -0500

    Prob Final update
    Most songs work well, there are some errors in which the input has to be specific and added an option to open the music video of the song which works most of the time.

--- scraper.py
+++ scraper.py
@@ -1,23 +1,24 @@
 # google library to search songs
 from googlesearch import search
-# beautiful soup library to make html file readable
-from bs4 import BeautifulSoup
-# urllib lib to request document behind url
-from urllib.request import urlopen
 
 
 # looks up song name on google
 def google_search(look):
-    for j in search(look, tld="com", num=1, stop=1, pause=2):
+    for i in search(look, tld="com", num=1, stop=1, pause=2):
+
+        # beautiful soup library to make html file readable
+        from bs4 import BeautifulSoup
+
+        # urllib lib to request document behind url
+        from urllib.request import urlopen
 
         # "prettifies" it (makes it readable) and prints)
         # uses bs4 to open the url after urllib gets the doc
-        soup = BeautifulSoup(urlopen(j), 'html.parser')
+        soup = BeautifulSoup(urlopen(i), 'html.parser')
     # uses bs4 find all method to extract text from div containing lyrics
     div = soup.find_all("div", {"class": None})
     # converts div text to string and passes into remove_html_tags
-    div = str(div)
-    remove_html_tags(div)
+    remove_html_tags(str(div))
 
 
 def remove_html_tags(refine):
@@ -27,15 +28,29 @@ def remove_html_tags(refine):
     print(re.sub(clean, '', refine))
 
 
+def youtube_link(ytfind):
+    import webbrowser
+    for i in search(ytfind, tld="com", num=1, stop=1, pause=2):
+        webbrowser.open(i, new=2)
+
+
 # song name to search and uses azlyrics.com (exclusively)
 print("Enter song name and artist: ")
 query = input()
+queryhold = query + " audio music video"
 query += ' azlyrics'
 # pass into function to search up lyrics
-google_search(query)
+print("Do you want to watch the music video?(y/n)")
+video = input()
 
-
-
+if video == 'n':
+    google_search(query)
+elif video == 'y':
+    google_search(query)
+    youtube_link(queryhold)
+else:
+    print("Error")
+    exit()
 
 
 
