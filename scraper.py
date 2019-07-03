4c7224921ce302c7a14ad6144c8e3e7437f45cec
Adrian <37904908+adriancerejo@users.noreply.github.com>
2019-07-03 11:46:06 -0400

    *Update*
    -Added a loop to keep the program going until user ends it
    -Added a clear function to empty the terminal before using program

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 # google library to search songs
 from googlesearch import search
-
+import os
 
 # looks up song name on google
 def google_search(look):
@@ -26,36 +26,37 @@ def remove_html_tags(refine):
     import re
     clean = re.compile('<.*?>')
     print(re.sub(clean, '', refine))
-    if video == 'n':
-        close_prompt = input("Press enter to exit")
 
 
 def youtube_link(ytfind):
     import webbrowser
     for i in search(ytfind, tld="ca", num=1, stop=1, pause=2):
         webbrowser.open(i, new=2)
-    close_prompt = input("Press enter to exit")
 
 
-# song name to search and uses azlyrics.com (exclusively)
-print("Enter song name and artist: ")
-query = input()
-queryhold = query + " audio music video"
-query += " site:azlyrics.com"
-# pass into function to search up lyrics
-print("Do you want to listen to the song?(y/n)")
-video = input()
+def requestSong():
+    # song name to search and uses azlyrics.com (exclusively)
+    print("Enter song name and artist: ")
+    name = input()
+    ytSearch = name + " audio music video"
+    name += " site:azlyrics.com"
+    # pass into function to search up lyrics
+    print("Do you want to listen to the song?(y/n)")
+    video = input()
 
-if video == 'n':
-    google_search(query)
-elif video == 'y':
-    google_search(query)
-    youtube_link(queryhold)
-else:
-    print("Error")
-    close_prompt = input("Press enter to exit")
+    if video == 'n':
+        google_search(name)
+    elif video == 'y':
+        google_search(name)
+        youtube_link(ytSearch)
 
 
-
-
-
+while 1:
+    cont = input("To use type 's' and to exit hit enter.\n")
+    if cont == 's':
+        os.system('clear')
+        os.system('cls')
+        requestSong()
+    else:
+        os.system('clear')
+        break
