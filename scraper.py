332115524076ffa83db1d1aab44cbed91292f64a
Adrian <37904908+adriancerejo@users.noreply.github.com>
2019-08-12 13:57:54 -0400

    -added comments

--- scraper.py
+++ scraper.py
@@ -1,43 +1,55 @@
-import os
+# import search.py to use its methods
 import search as song
+# import tkinter for the gui
 import tkinter as tk
 from tkinter import *
 
+# create a global variable to store lyrics
 lyrics = ""
       
+# create tkinter objects
 root = tk.Tk()
 s = tk.Scrollbar(root)
 
+# set main window dimensions
 root.geometry("1200x800") # Width X Height
 
+# create title label
 lblTitle = tk.Label(root, text='Lyrics Scraper')
 lblTitle.place(x= 24, y=10)
 lblTitle.config(font=('helvetica', 14))
 
+# create label
 lblSongName = tk.Label(root, text='Enter Song:')
 lblSongName.place(x= 24, y=35)
 lblSongName.config(font=('helvetica', 10))
 
-
+# create textbox
 txtSongName = tk.Entry (root) 
 txtSongName.place(x=24, y=55)
 
-def getSong(songName):
+
+def getSong():
+    # get input from textbox
+    songName = txtSongName.get()
+
+    # function to allow user to scroll up
     def on_mousewheelup(event):
         canvas.yview_scroll(-1, "units")
-    
+    # function to allow user to scroll down
     def on_mousewheeldown(event):
         canvas.yview_scroll(1, "units")
 
-
+    # passes song name with additional string at end into google search method from search module
     lyrics = song.searchSong.google_search(songName + " site:azlyrics.com")
+    # create the canvas that will show the lyrics and position it
     canvas = Canvas(root, width=500, height=600, yscrollcommand=s.set)
     canvas.place(relx=0.02, rely=.15)
-    canvas.create_text(10, 10, anchor=NW, text=lyrics)
-                
-    canvas.bind_all("<Button-5>", on_mousewheeldown)
+    canvas.create_text(10, 0, anchor=NW, text=lyrics)
+    # listen for scrolls to move window up/down
     canvas.bind_all("<Button-4>", on_mousewheelup)
-
+    canvas.bind_all("<Button-5>", on_mousewheeldown)
+    # other scroll commands
     s.config(command=canvas.yview)
     s.pack(side=tk.RIGHT, fill=tk.Y)
     canvas.config(scrollregion=canvas.bbox("all"))
@@ -45,13 +57,11 @@ def getSong(songName):
     # ytSearch = name + " official music video"
     # song.searchSong.youtube_link(ytSearch)
 
-def requestSong():
-    songName = txtSongName.get()
-    getSong(songName)
 
-
-btnSearch = tk.Button(text='Get Lyrics', command=requestSong,  bg='brown', fg='white', font=('helvetica', 9, 'bold'))
+# button to search the lyrics; will call requestSong function on click
+btnSearch = tk.Button(text='Get Lyrics', command=getSong,  bg='brown', fg='white', font=('helvetica', 9, 'bold'))
 btnSearch.place(x=24, y=85)
 
+# REQUIRED for tkinter
 root.mainloop()
 
--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 # google library to search songs
 from googlesearch import search
+# global variable that will store the lyrics
 songLyrics = ''
 
 
@@ -20,25 +21,30 @@ class searchSong:
                 soup = BeautifulSoup(urlopen(i), 'html.parser')
             # uses bs4 find all method to extract text from div containing lyrics
             div = soup.find_all("div", {"class": None})
-            # converts div text to string and passes into remove_html_tags
+            # converts div text to string and passes into remove_html_tags and return
             return searchSong.remove_html_tags(str(div))
         except:
+            # in the case of an error, return an error message
             songLyrics = "Error: Song Not Found \n(Check spelling/Enter Artist Name)"
-            return songLyrics
+            return str(songLyrics)
     
     def remove_html_tags(refine):
         # removes html tags from string using regex
         import re
         clean = re.compile('<.*?>')
         lyrics = re.sub(clean, '', refine)
+        # convert self to string and remove any other characters
         lyrics = str(lyrics)
         lyrics = lyrics.replace('[', '')
         lyrics = lyrics.replace(']', '')
         lyrics = lyrics.replace(',', '', 1)
+        # set the global variable as the lyrics
         songLyrics = lyrics
+        # return the lyrics as a string
         return str(songLyrics)
-        
-    def youtube_link(ytfind):
-        import webbrowser
-        for i in search(ytfind, tld="ca", num=1, stop=1, pause=2):
-            webbrowser.open(i, new=2)
+      
+    #### TODO: ADD A YOUTUBE PLAYER WITHIN THE APPLICATION WINDOW ####   
+    # def youtube_link(ytfind):
+    #     import webbrowser
+    #     for i in search(ytfind, tld="ca", num=1, stop=1, pause=2):
+    #         webbrowser.open(i, new=2)
