c1d7c24cf562bf7a9625b2c1f29e0c37968b9319
Adrian <37904908+adriancerejo@users.noreply.github.com>
2019-08-12 13:42:48 -0400

    -updates lyrics properly and scrollwheel works

--- scraper.py
+++ scraper.py
@@ -3,16 +3,12 @@ import search as song
 import tkinter as tk
 from tkinter import *
 
-
 lyrics = ""
       
 root = tk.Tk()
 s = tk.Scrollbar(root)
 
 root.geometry("1200x800") # Width X Height
-
-canvas = Canvas(root, width=500, height=600, yscrollcommand=s.set)
-
 
 lblTitle = tk.Label(root, text='Lyrics Scraper')
 lblTitle.place(x= 24, y=10)
@@ -26,24 +22,24 @@ lblSongName.config(font=('helvetica', 10))
 txtSongName = tk.Entry (root) 
 txtSongName.place(x=24, y=55)
 
-def on_mousewheelup(event):
-    canvas.yview_scroll(-1, "units")
-
-def on_mousewheeldown(event):
-    canvas.yview_scroll(1, "units")
-    
-    
-canvas.bind_all("<Button-5>", on_mousewheeldown)
-canvas.bind_all("<Button-4>", on_mousewheelup)
-
 def getSong(songName):
+    def on_mousewheelup(event):
+        canvas.yview_scroll(-1, "units")
+    
+    def on_mousewheeldown(event):
+        canvas.yview_scroll(1, "units")
+
 
     lyrics = song.searchSong.google_search(songName + " site:azlyrics.com")
-    s.config(command=canvas.yview)
-    s.pack(side=tk.RIGHT, fill=tk.Y) 
-    canvas.create_text(10, 10, anchor=NW, text=lyrics)
+    canvas = Canvas(root, width=500, height=600, yscrollcommand=s.set)
     canvas.place(relx=0.02, rely=.15)
+    canvas.create_text(10, 10, anchor=NW, text=lyrics)
+                
+    canvas.bind_all("<Button-5>", on_mousewheeldown)
+    canvas.bind_all("<Button-4>", on_mousewheelup)
 
+    s.config(command=canvas.yview)
+    s.pack(side=tk.RIGHT, fill=tk.Y)
     canvas.config(scrollregion=canvas.bbox("all"))
     
     # ytSearch = name + " official music video"
@@ -52,11 +48,6 @@ def getSong(songName):
 def requestSong():
     songName = txtSongName.get()
     getSong(songName)
-
-    # canvas.create_text(10, 90, text='something')
-    # canvas.focus_set()
-    # canvas.pack(expand=YES, fill=BOTH)
-    # root.mainloop()
 
 
 btnSearch = tk.Button(text='Get Lyrics', command=requestSong,  bg='brown', fg='white', font=('helvetica', 9, 'bold'))
